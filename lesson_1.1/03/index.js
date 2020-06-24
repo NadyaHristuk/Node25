84443d18f6b83fb9a1da9d4ad03b5585385288d9
Nadya Hristuk <elhe@i.ua>
2020-06-24 18:38:41 +0300

    Add files via upload

---
+++ lesson_1.1/03/index.js
@@ -0,0 +1,14 @@
+let colors = require('colors');
+let text = 'Node is geate! 111123'
+
+// console.log(text.rainbow);
+
+// console.log(process.execPath);
+// console.log(process.version);
+// console.log(process.platform);
+// console.log(process.arch);
+// console.log(process.title);
+
+
+// console.log(__dirname);
+// console.log(__filename);
